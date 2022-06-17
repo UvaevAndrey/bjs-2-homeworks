003752485cabe24cb61dc091f720bedb7f6aaf6d
Уваев Андрей <motogp@bk.ru>
2022-06-17 17:44:54 +0300

    1.base-conceptsv/1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,39 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let d = (Math.pow(b,2) - 4*a*c);
+    if (d < 0) {
+      arr = [];
+    } else if (d === 0) {
+      arr = [(-b/(2*a))];
+    } else {
+      arr = [((-b + Math.sqrt(d) )/(2*a)),((-b - Math.sqrt(d) )/(2*a))];
+    }
+      return arr;
 }
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let months;
 
-  // код для задачи №2 писать здесь
+  function monthDiff(d1, d2) {
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth();
+    months += d2.getMonth();
+    return months <= 0 ? 0 : months;
+  }
 
-  return totalAmount;
+  
+      if (isFinite (percent) === false) {
+        return totalAmount = `Параметр "${window.percent.placeholder}" содержит неправильное значение "${percent}"`;
+      } else if (isFinite (contribution) === false) {
+        return totalAmount = `Параметр "${window.contribution.placeholder}" содержит неправильное значение "${contribution}"`;
+      } else if (isFinite (amount) === false) {
+        return totalAmount = `Параметр "${window.amount.placeholder}" содержит неправильное значение "${amount}"`;
+      } else monthDiff(new Date(), date)
+      totalAmount = (Number(amount) - (Number(contribution)))*(Number(percent)/100/12 + (Number(percent)/100/12/(((1 + Number(percent)/100/12)**months) - 1)))*months
+      return totalAmount.toFixed(2)
 }
