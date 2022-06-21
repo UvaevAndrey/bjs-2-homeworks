89797d1bd731668a163a63fb4bd9348e73442e0c
Уваев Андрей <motogp@bk.ru>
2022-06-21 22:11:13 +0300

    leson 2+3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,55 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+  min = arr[0];
+  max = min;
+  sum = 0;
+  n = 0;
+  for (i=0; i < arr.length ; ++i){
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+    sum += arr[i];
+    n++;
+  }
+  avg = Number((sum/n).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
+
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (i=0; i < arr.length; i++){
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0
 
-  // Ваш кода
-  // for ...
-  
+   for (j=0; j < arrOfArr.length ; j++){
+    if (func(arrOfArr[j]) > max) max = func(arrOfArr[j]) 
+  }
   return max;
 }
 
+
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min1, max1, res
+  min1 = arr[0];
+  max1 = min1;
+    
+  for (q=0; q < arr.length ; ++q){
+    if (arr[q] > max1) max1 = arr[q];
+    if (arr[q] < min1) min1 = arr[q];
+    res = Math.abs(min1 - max1)
+  }
+  return res;
 }
+
+
+
+
+
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.length === arr2.length && arr1.every ((n,i) => n === arr2[i]) 
+ return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+   
+  let resultArr = arr.filter((n) => n>0).filter((x) => x % 3 === 0).map(i => i*10);
 
   return resultArr; // array
 }
